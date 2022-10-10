9f3dbfae07e522a3d3ccec8eaa75cf4c2de85e25
neirmora <neirmora@gmail.com>
2022-10-10 01:33:31 -0300

    Ventanas emergentes y media q

--- app.js
+++
@@ -1,5 +0,0 @@
-const btnMenu = document.querySelector(".btnMenu");
-const menuItem = document.querySelector(".menuItem");
-btnMenu.addEventListener("click", function(){
-    menuItem.classList.toggle("mostrar");
-});
