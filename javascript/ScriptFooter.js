53b1ce4fdc4a811215c978f65cc6b293cfa03fb3
DaiYanibb <115119548+DaiYanibb@users.noreply.github.com>
2022-10-15 14:36:15 -0300

    Se corrige css de Footer
    Se corrige CSS de Footer para que quede más parejo. Se deja comentado el código anterior.

--- javascript/ScriptFooter.js
+++ javascript/ScriptFooter.js
@@ -1,8 +1,8 @@
-var Idfooter=`<p>Pisar La Tierra <i class="fa-regular fa-copyright"></i> copyright 2022</p>
+var Idfooter=`<div><p>Pisar La Tierra <i class="fa-regular fa-copyright"></i> copyright 2022</p></div>
     
-<a href="#" class="facebook"><i class="fa-brands fa-square-facebook"></i> Facebook</a>
-<a href="#" class="instagram"><i class="fa-brands fa-square-instagram"></i> Instagram</a>
-<a href="https://web.whatsapp.com/" class="whatsapp"><i class="fa-brands fa-whatsapp"></i>
+<a href="#"><i class="fa-brands fa-square-facebook"></i> Facebook</a>
+<a href="#"><i class="fa-brands fa-square-instagram"></i> Instagram</a>
+<a href="https://web.whatsapp.com/"><i class="fa-brands fa-whatsapp"></i>
     Whatsapp</a>`
 
 document.getElementById("Idfooter").innerHTML=Idfooter
