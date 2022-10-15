9c38d814762dfed0238066c97da6577b43d8b02c
DaiYanibb <115119548+DaiYanibb@users.noreply.github.com>
2022-10-15 13:32:36 -0300

    Footer Java
    Prueba de footer con Java

---
+++ javascript/ScriptFooter.js
@@ -0,0 +1,10 @@
+var Idfooter=`<p>Pisar La Tierra <i class="fa-regular fa-copyright"></i> copyright 2022</p>
+    
+<a href="#" class="facebook"><i class="fa-brands fa-square-facebook"></i> Facebook</a>
+<a href="#" class="instagram"><i class="fa-brands fa-square-instagram"></i> Instagram</a>
+<a href="https://web.whatsapp.com/" class="whatsapp"><i class="fa-brands fa-whatsapp"></i>
+    Whatsapp</a>`
+
+document.getElementById("Idfooter").innerHTML=Idfooter
+
+
